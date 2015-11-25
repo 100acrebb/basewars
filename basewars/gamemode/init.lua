67096a4cf2a3c19ab3cac55498a9614a1a84a3ea
CaveeJohnson <caveejohnson@hotmail.com>
2015-11-25 19:53:24 +0000

    UTIL, New language Strings, todo

--- basewars/gamemode/init.lua
+++ basewars/gamemode/init.lua
@@ -1,2 +1,24 @@
 include("include.lua")
 AddCSLuaFile("include.lua")
+
+local tag = "BaseWars.UTIL"
+
+BaseWars.UTIL = {}
+
+function BaseWars.UTIL.Log(...)
+	
+	MsgC("[BASEWARS] ", ...)
+	MsgN("")
+	
+end
+
+function BaseWars.UTIL.CleanupMap()
+
+	for k, v in next, ents.FindByClass("game_text") do
+	
+		SafeRemoveEntity(v)
+	
+	end
+	
+end
+hook.Add("InitPostEntity", tag .. ".CleanupMap", BaseWars.UTIL.CleanupMap)
--- basewars/gamemode/language.lua
+++ basewars/gamemode/language.lua
@@ -1,5 +1,9 @@
 BaseWars.LANG = {
-	FactionNameTaken 	= "This faction name is already in use!",
-	FactionNotExist		= "That faction does not exist!",
-	FactionCantDisband	= "Only the faction leader can disband the faction!",
+	FactionNameTaken 		= "This faction name is already in use!",
+	FactionNotExist			= "That faction does not exist!",
+	FactionCantDisband		= "Only the faction leader can disband the faction!",
+	FactionWrongPass		= "That is not the correct password!",
+	FactionCantLeaveLeader	= "You cannot leave the faction as its leader, you must disband it!",
+	
+	
 }
