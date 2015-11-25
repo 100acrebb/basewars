4f0eeb1b5b6c7c0d38aee5879eab763840ea1184
CaveeJohnson <caveejohnson@hotmail.com>
2015-11-25 15:13:03 +0000

    Faction System Prototype

--- basewars/gamemode/shared/factions.lua
+++ basewars/gamemode/shared/factions.lua
@@ -3,7 +3,44 @@ BaseWars.Factions = {}
 local tag = "BaseWars.Factions"
 local PLAYER = debug.getregistry().Player
 
-BaseWars.Factions.FactionTable = {}
+if SERVER then
+
+	util.AddNetworkString(tag)
+
+	BaseWars.Factions.FactionTable = {}
+	
+	function BaseWars.Factions.HandleNetMessage(ply, len)
+	
+		local Mode = net.ReadString()
+		
+		if Mode == "Set" then
+			
+			local value = net.ReadString()
+			local password = net.ReadString()
+			
+			BaseWars.Factions.Set(ply, value, password, false)
+		
+		elseif Mode == "Leave" then
+		
+			local disband = net.ReadBool()
+		
+			BaseWars.Factions.Leave(password, disband)
+		
+		elseif Mode == "Create" then
+		
+			local value = net.ReadString()
+			local password = net.ReadString()
+			
+			if password:Trim() == "" then password = nil end
+			
+			BaseWars.Factions.Create(ply, value, password)
+			
+		end
+		
+	end
+	net.Receive(tag, BaseWars.Factions.HandleNetMessage)
+	
+end
 
 function BaseWars.Factions.Get(ply)
 
@@ -12,17 +49,9 @@ function BaseWars.Factions.Get(ply)
 end
 PLAYER.GetFaction = BaseWars.Factions.Get
 
-function BaseWars.Factions.Set(ply, value)
+function BaseWars.Factions.Set(ply, value, password, force)
 
-	if CLIENT then
-	
-		ErrorNoHalt("Cannot set Faction clientside.")
-	
-		return
-	
-	end
-
-	if not value then
+	if not value or not isstring(value) then
 	
 		ErrorNoHalt("Error setting Faction, invalid value.")
 		debug.Trace()
@@ -31,14 +60,53 @@ function BaseWars.Factions.Set(ply, value)
 	
 	end
 
+	if CLIENT then
+	
+		net.Start(tag)
+			net.WriteString("Set")
+			net.WriteString(value)
+			net.WriteString(password)
+		net.SendToServer()
+	
+		return
+	
+	end
+	
+	local Table = BaseWars.Factions.FactionTable
+	local Faction = Table[value]
+	
+	if not Faction then
+	
+		BaseWars.Util_Player.Notification(ply, BaseWars.LANG.FactionNotExist, BASEWARS_NOTIFICATION_ERROR)
+	
+		return
+		
+	end
+	
+	if Faction.password and Faction.password ~= password and not force then
+	
+		BaseWars.Util_Player.Notification(ply, BaseWars.LANG.FactionWrongPass, BASEWARS_NOTIFICATION_ERROR)
+	
+		return
+	
+	end
+	
+	
+	Faction.members[ply:SteamID()] = ply
+	ply:SetNWString(tag, value)
+
 end
 PLAYER.SetFaction = BaseWars.Factions.Set
+PLAYER.JoinFaction = BaseWars.Factions.Set
 
-function BaseWars.Factions.Leave(ply, disband)
+function BaseWars.Factions.Leave(ply, disband, forcedisband)
 
 	if CLIENT then
 	
-		-- net message
+		net.Start(tag)
+			net.WriteString("Leave")
+			net.WriteBool(disband)
+		net.SendToServer()
 		
 		return
 		
@@ -56,7 +124,7 @@ function BaseWars.Factions.Leave(ply, disband)
 		
 	end
 
-	if disband and (Faction.leader ~= ply:SteamID() or not ply:IsAdmin()) then
+	if not forcedisband and disband and (Faction.leader ~= ply:SteamID() or not ply:IsAdmin()) then
 	
 		BaseWars.Util_Player.Notification(ply, BaseWars.LANG.FactionCantDisband, BASEWARS_NOTIFICATION_ERROR)
 		
@@ -64,7 +132,9 @@ function BaseWars.Factions.Leave(ply, disband)
 		
 	end
 	
-	if disband then
+	if forcedisband or disband then
+	
+		BaseWars.UTIL.Log("Faction disband for ", Fac, ". ", #Faction.members, " members.")
 	
 		for k, v in next, Faction.members do
 			
@@ -75,6 +145,14 @@ function BaseWars.Factions.Leave(ply, disband)
 		Table[Fac] = nil
 		
 	else
+	
+		if Faction.leader == ply:SteamID() then
+		
+			BaseWars.Util_Player.Notification(ply, BaseWars.LANG.FactionCantLeaveLeader, BASEWARS_NOTIFICATION_ERROR)
+			
+			return
+			
+		end
 		
 		ply:SetNWString(tag, "")
 		
@@ -83,8 +161,20 @@ function BaseWars.Factions.Leave(ply, disband)
 	end
 
 end
+PLAYER.LeaveFaction = BaseWars.Factions.Leave
 
-function BaseWars.Factions.Create(ply, name, password)
+function BaseWars.Factions.Clean(ply)
+
+	local Table = BaseWars.Factions.FactionTable
+	local Fac = ply:GetFaction()
+	local Faction = Table[Fac]
+	
+	BaseWars.Factions.Leave(ply, Faction.leader == ply:SteamID())
+	
+end
+hook.Add("PlayerDisconnect", tag .. ".Clean", BaseWars.Factions.Clean)
+
+function BaseWars.Factions.Create(ply, name, password, color)
 
 	if not name or not istring(name) or (password and not isstring(password)) then
 	
@@ -97,7 +187,11 @@ function BaseWars.Factions.Create(ply, name, password)
 	
 	if CLIENT then
 	
-		-- send net message
+		net.Start(tag)
+			net.WriteString("Create")
+			net.WriteString(name)
+			net.WriteString(password or "")
+		net.SendToServer()
 		
 		return
 		
@@ -113,7 +207,15 @@ function BaseWars.Factions.Create(ply, name, password)
 		
 	end
 	
-	Table[name] = {leader = ply:SteamID(), password = password, members = {}}
+	BaseWars.UTIL.Log("Faction created for ", name, ". Leader: ", ply:Nick(), ". Password ", password, ".")
+	
+	Table[name] = {
+		leader = ply:SteamID(),
+		password = password,
+		members = {},
+		color = color or color_white,
+	}
+		
 	ply:SetFaction(name)
 
 end
