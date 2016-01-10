334613e35da6338db32154c9bc8965267d468140
TrenchFroast <thesteva256@gmail.com>
2016-01-10 02:59:13 +0000

    Update cl_interactions.lua
    no trix, spaces in brackets does not make the code more readable
    no trix, that's not how yu use "next"
    no trix, using a Color table with surface.Set*Color is not optimal
    no trix, you don't need extra brackets with "x and y or z"

--- basewars/gamemode/client/cl_interactions.lua
+++ basewars/gamemode/client/cl_interactions.lua
@@ -1,11 +1,12 @@
---JUST DONT EDIT THIS WITH REASON FOR STYLING PLEASE.
+--JUST DONT EDIT THIS WITH REASON FOR STYLING PLEASE. - Trix
+--does this count -Ghosty
 LookEnt = {}
 
-LookEnt.Key = Material( "custom/key.png" )
+LookEnt.Key = Material("custom/key.png")
 LookEnt.Dist = 150
 
 LookEnt.ConVarName = "interactions_enabled"
-LookEnt.ConVar = CreateConVar( LookEnt.ConVarName, 1, FCVAR_ARCHIVE, "Enable the indicators for entities? 1 = Enabled, 0 = Disabled" )
+LookEnt.ConVar = CreateConVar(LookEnt.ConVarName, 1, FCVAR_ARCHIVE, "Enable the indicators for entities? 1 = Enabled, 0 = Disabled")
 
 LookEnt.Ents = {}
 
@@ -21,36 +22,38 @@ surface.CreateFont("LookEnt.Text", {
 	weight = 400,
 })
 
-function LookEnt:RegisterEnt( class, key, action, color, cansee )
-	key = string.upper( key )
-	cansee = cansee or function( aimEnt ) return true end
-	table.insert( LookEnt.Ents, {class=class, key=key, action=action, color=color, cansee=cansee } )
+function LookEnt:RegisterEnt(class, key, action, color, cansee)
+	key = string.upper(key)
+	cansee = cansee or function(aimEnt) return true end
+	LookEnt.Ents[#LookEnt.Ents + 1] = {class = class, key = key, action = action, color = color, cansee = cansee}
 end
 
 local me = LocalPlayer()
 
 function LookEnt:Paint()
-	if not IsValid( me ) then me = LocalPlayer() end
+	if not IsValid(me) then me = LocalPlayer() end
+	if not IsValid(me) then return end
 	if not tobool(LookEnt.ConVar:GetInt()) then return end
 	local aimEnt = me:GetEyeTrace().Entity
-	if not BaseWars.Ents:Valid( aimEnt ) then return end
+	if not BaseWars.Ents:Valid(aimEnt) then return end
 	if aimEnt:GetClass() == "worldspawn" then return end
-	for _, tbl, next in pairs( LookEnt.Ents ) do
+
+	for _, tbl in next, LookEnt.Ents do
 		local class = tbl.class
 		
-		if type( class ) == "string" then
+		if type(class) == "string" then
 			if aimEnt:GetClass() ~= class then continue end	
 		else
 			if not class(aimEnt) then continue end
 		end
 		
-		if not tbl.cansee( aimEnt ) then continue end
+		if not tbl.cansee(aimEnt) then continue end
 		
-		if aimEnt:GetPos():Distance( me:GetPos() ) > LookEnt.Dist then continue end
+		if aimEnt:GetPos():Distance(me:GetPos()) > LookEnt.Dist then continue end
 		
 		local key = tbl.key
-		local action, name = tbl.action( aimEnt )
-		local actioncolor, namecolor = tbl.color( aimEnt )
+		local action, name = tbl.action(aimEnt)
+		local actioncolor, namecolor = tbl.color(aimEnt)
 		
 		local sW = ScrW()
 		local sH = ScrH()
@@ -58,119 +61,124 @@ function LookEnt:Paint()
 		--added spacing here instead
 		action = "   ".. action .." "
 		
-		surface.SetFont( "LookEnt.Key" )
-		local keyX, keyY = surface.GetTextSize( key )
+		surface.SetFont("LookEnt.Key")
+		local keyX, keyY = surface.GetTextSize(key)
 		
-		surface.SetFont( "LookEnt.Text" )
-		local actionX, actionY = surface.GetTextSize( action )
+		surface.SetFont("LookEnt.Text")
+		local actionX, actionY = surface.GetTextSize(action)
 		
-		surface.SetFont( "LookEnt.Text" )
-		local nameX, nameY = surface.GetTextSize( name )
+		surface.SetFont("LookEnt.Text")
+		local nameX, nameY = surface.GetTextSize(name)
 		
-		surface.SetMaterial( LookEnt.Key )
-		surface.SetDrawColor( Color( 255, 255, 255 ) )
-		surface.DrawTexturedRect( sW / 2 - keyX/2 - 20 - actionX/2 , sH / 2 + 40 / 2, 40, 40 )
+		surface.SetMaterial(LookEnt.Key)
+		surface.SetDrawColor(255, 255, 255, 255)
+		surface.DrawTexturedRect(sW / 2 - keyX/2 - 20 - actionX/2 , sH / 2 + 40 / 2, 40, 40)
 		
-		surface.SetFont( "LookEnt.Key" )
-		surface.SetTextColor( Color( 0, 0, 0 ) )
-		surface.SetTextPos( sW / 2 - keyX - actionX/2 + 1 , sH / 2 + 40 / 2 + 40 / 2 - keyY / 2 + 1 )
-		surface.DrawText( key )
+		surface.SetFont("LookEnt.Key")
+		surface.SetTextColor(0, 0, 0, 255)
+		surface.SetTextPos(sW / 2 - keyX - actionX/2 + 1 , sH / 2 + 40 / 2 + 40 / 2 - keyY / 2 + 1)
+		surface.DrawText(key)
 		
-		surface.SetFont( "LookEnt.Key" )
-		surface.SetTextColor( Color( 0, 0, 0 ) )
-		surface.SetTextPos( sW / 2 - keyX - actionX/2 , sH / 2 + 40 / 2 + 40 / 2 - keyY / 2 )
-		surface.DrawText( key )
+		surface.SetFont("LookEnt.Key")
+		surface.SetTextColor(0, 0, 0, 255)
+		surface.SetTextPos(sW / 2 - keyX - actionX/2 , sH / 2 + 40 / 2 + 40 / 2 - keyY / 2)
+		surface.DrawText(key)
 		
-		surface.SetFont( "LookEnt.Text" )
-		surface.SetTextColor( Color( 0, 0, 0 ) )
-		surface.SetTextPos( sW / 2 - actionX/2 + 1 , sH / 2 + 40 / 2 + 40 / 2 - actionY / 2 + 1 )
-		surface.DrawText( action )
+		surface.SetFont("LookEnt.Text")
+		surface.SetTextColor(0, 0, 0, 255)
+		surface.SetTextPos(sW / 2 - actionX/2 + 1 , sH / 2 + 40 / 2 + 40 / 2 - actionY / 2 + 1)
+		surface.DrawText(action)
 		
-		surface.SetFont( "LookEnt.Text" )
-		surface.SetTextColor( actioncolor )
-		surface.SetTextPos( sW / 2 - actionX/2 , sH / 2 + 40 / 2 + 40 / 2 - actionY / 2 )
-		surface.DrawText( action )
+		surface.SetFont("LookEnt.Text")
+		surface.SetTextColor(actioncolor)
+		surface.SetTextPos(sW / 2 - actionX/2 , sH / 2 + 40 / 2 + 40 / 2 - actionY / 2)
+		surface.DrawText(action)
 		
-		surface.SetFont( "LookEnt.Text" )
-		surface.SetTextColor( Color( 0, 0, 0 ) )
-		surface.SetTextPos( sW / 2 + actionX /2 + 1 , sH / 2 + 40 / 2 + 40 / 2 - actionY / 2 + 1 )
-		surface.DrawText( name )
+		surface.SetFont("LookEnt.Text")
+		surface.SetTextColor(0, 0, 0, 255)
+		surface.SetTextPos(sW / 2 + actionX /2 + 1 , sH / 2 + 40 / 2 + 40 / 2 - actionY / 2 + 1)
+		surface.DrawText(name)
 		
-		surface.SetFont( "LookEnt.Text" )
-		surface.SetTextColor( namecolor )
-		surface.SetTextPos( sW / 2 + actionX /2 , sH / 2 + 40 / 2 + 40 / 2 - actionY / 2 )
-		surface.DrawText( name )
+		surface.SetFont("LookEnt.Text")
+		surface.SetTextColor(namecolor)
+		surface.SetTextPos(sW / 2 + actionX /2 , sH / 2 + 40 / 2 + 40 / 2 - actionY / 2)
+		surface.DrawText(name)
 	end
 end
 
-hook.Add( "HUDPaint", "LookEnt.Paint", LookEnt.Paint )
+hook.Add("HUDPaint", "LookEnt.Paint", LookEnt.Paint)
 
---The awfull part down here
-LookEnt:RegisterEnt( "prop_door_rotating", input.LookupBinding( "+use" ), function( aimEnt )
+local color1, color2 = Color(128, 255, 0), Color(255, 255, 21)
+local color3 = Color(255, 0, 0)
+
+--The awful part down here
+LookEnt:RegisterEnt("prop_door_rotating", input.LookupBinding("+use"), function(aimEnt)
 	return "Use", "door"
-end, 
-function( aimEnt )
-	return Color( 128, 255, 0 ), Color( 255, 255, 21 )
-end )
+end,
+function(aimEnt)
+	return color1, color2
+end)
 
-LookEnt:RegisterEnt( function( aimEnt ) 
+LookEnt:RegisterEnt(function(aimEnt)
 	return aimEnt.IsPrinter
-end, input.LookupBinding( "+use" ), 
-function( aimEnt )
+end, input.LookupBinding("+use"),
+
+function(aimEnt)
 	return "Collect", "money"
 end, 
-function( aimEnt )
-	return ((aimEnt:GetMoney() > 0) and Color( 128, 255, 0 )) or Color( 255, 0, 0 ), Color( 255, 255, 21 )
-end )
 
-LookEnt:RegisterEnt( "bw_spawnpoint", input.LookupBinding( "+use" ), function( aimEnt )
-	return (aimEnt:GetUsable() and "Activate") or "Look at", "spawnpoint"
-end, 
-function( aimEnt )
-	return Color( 128, 255, 0 ), Color( 255, 255, 21 )
+function(aimEnt)
+	return aimEnt:GetMoney() > 0 and color1 or color3, color2
+end)
+
+LookEnt:RegisterEnt("bw_spawnpoint", input.LookupBinding("+use"), function( aimEnt)
+	return aimEnt:GetUsable() and "Activate" or "Look at", "spawnpoint"
 end,
-function( aimEnt )
+function(aimEnt)
+	return color1, color2
+end,
+function(aimEnt)
 	return aimEnt:GetUsable()
-end )
+end)
 
-LookEnt:RegisterEnt( "bw_druglab", input.LookupBinding( "+use" ), function( aimEnt )
+LookEnt:RegisterEnt("bw_druglab", input.LookupBinding("+use"), function(aimEnt)
 	return "Use", "Drug Lab"
-end, 
-function( aimEnt )
-	return Color( 128, 255, 0 ), Color( 255, 255, 21 )
-end )
+end,
+function(aimEnt)
+	return color1, color2
+end)
 
-LookEnt:RegisterEnt( "bw_dispenser_armor", input.LookupBinding( "+use" ), function( aimEnt )
+LookEnt:RegisterEnt("bw_dispenser_armor", input.LookupBinding("+use"), function(aimEnt)
 	return "Use", "Armor dispenser"
 end, 
-function( aimEnt )
-	return (aimEnt:GetUsable() and Color( 128, 255, 0 )) or Color( 255, 0, 0 ), Color( 255, 255, 21 )
-end )
+function(aimEnt)
+	return aimEnt:GetUsable() and color1 or color3, color2
+end)
 
-LookEnt:RegisterEnt( "bw_dispenser_paper", input.LookupBinding( "+use" ), function( aimEnt )
+LookEnt:RegisterEnt("bw_dispenser_paper", input.LookupBinding("+use"), function(aimEnt)
 	return "Use", "Paper dispenser"
 end, 
-function( aimEnt )
-	return (aimEnt:GetUsable() and Color( 128, 255, 0 )) or Color( 255, 0, 0 ), Color( 255, 255, 21 )
-end )
+function(aimEnt)
+	return aimEnt:GetUsable() and color1 or color3, color2
+end)
 
-LookEnt:RegisterEnt( "bw_dispenser_ammo", input.LookupBinding( "+use" ), function( aimEnt )
+LookEnt:RegisterEnt("bw_dispenser_ammo", input.LookupBinding("+use"), function(aimEnt)
 	return "Use", "Ammo dispenser"
 end, 
-function( aimEnt )
-	return (aimEnt:GetUsable() and Color( 128, 255, 0 )) or Color( 255, 0, 0 ), Color( 255, 255, 21 )
-end )
+function(aimEnt)
+	return aimEnt:GetUsable() and color1 or color3, color2
+end)
 
-LookEnt:RegisterEnt( "bw_vendingmachine", input.LookupBinding( "+use" ), function( aimEnt )
+LookEnt:RegisterEnt("bw_vendingmachine", input.LookupBinding("+use"), function(aimEnt)
 	return "Use", "Vending machine"
 end, 
-function( aimEnt )
-	return (aimEnt:GetUsable() and Color( 128, 255, 0 )) or Color( 255, 0, 0 ), Color( 255, 255, 21 )
-end )
+function(aimEnt)
+	return aimEnt:GetUsable() and color1 or color3, color2
+end)
 
-LookEnt:RegisterEnt( "bw_drink_drug", input.LookupBinding( "+use" ), function( aimEnt )
+LookEnt:RegisterEnt("bw_drink_drug", input.LookupBinding("+use"), function(aimEnt)
 	return "Use", aimEnt:GetDrugEffect() or ""
 end, 
-function( aimEnt )
-	return Color( 128, 255, 0 ), Color( 255, 255, 21 )
-end )
+function(aimEnt)
+	return aimEnt:GetUsable() and color1 or color3, color2
+end)
